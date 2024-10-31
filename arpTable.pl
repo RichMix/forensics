45868a5a9f6fb2de0928849b7ee74490841c5723
ChoppSueyy <82078286+RichMix@users.noreply.github.com>
2024-10-31 15:04:36 -0700

    ARP Table example
    Walking through GPT solutions

---
+++ arpTable.pl
@@ -0,0 +1,31 @@
+please read IP address       HW type     Flags       HW address            Mask     Device
+10.0.0.5      0x1         0x6         0a:54:5f:54:a1:91     *        eth0
+10.0.0.250    0x1         0x6         0a:72:dd:13:77:fd     *        eth0
+10.0.0.31     0x1         0x2         0a:55:77:e4:b9:23     *        eth0
+10.0.0.21     0x1         0x2         7a:93:06:fe:85:a3     *        eth0
+10.0.0.20     0x1         0x2         a2:1f:1e:8a:b4:46     *        eth0
+10.0.0.24     0x1         0x2         bc:1c:da:9f:df:37     *        eth0
+10.0.0.22     0x1         0x2         05:4c:5b:35:be:de     *        eth0
+10.0.0.25     0x1         0x2         b7:13:fc:0c:25:73     *        eth0
+10.0.0.27     0x1         0x2         9e:86:82:e1:ae:bb     *        eth0
+10.0.0.26     0x1         0x2         4a:47:52:9a:6c:0d     *        eth0
+10.0.0.30     0x1         0x2         c0:1c:c3:35:ba:34     *        eth0
+10.0.0.31     0x1         0x2         ef:5d:de:57:ed:02     *        eth0
+10.0.0.29     0x1         0x2         dc:a8:c6:d5:60:41     *        eth0
+10.0.0.28     0x1         0x2         fd:95:98:56:b3:64     *        eth0
+10.0.0.45     0x1         0x2         a2:1f:1e:8a:b4:46     *        eth0
+10.0.0.42     0x1         0x2         1c:c5:d5:f8:66:dd     *        eth0
+10.0.0.40     0x1         0x6         93:aa:44:dc:33:73     *        eth0
+10.0.0.41     0x1         0x2         0e:be:7c:58:12:4e     *        eth0
+10.0.0.43     0x1         0x2         1f:cb:d7:f0:37:84     *        eth0
+
+
+# Challenge questions (Fall NCL)
+# Q1 - 10 points
+# How many IP addresses are in this ARP table?
+	
+# Q2 - 20 points
+# How many entries are marked as automatically complete?
+	
+# Q3 - 70 points
+# Which MAC address looks to be involved in an ARP spoofing attack?
